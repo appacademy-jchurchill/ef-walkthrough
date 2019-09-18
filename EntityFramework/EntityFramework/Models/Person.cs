2db64e0093d68d36a762dc24d60d2d17f2c4b3e3
James Churchill <james@smashdev.com>
2019-09-18 08:08:19 -0700

    Various updates

--- EntityFramework/EntityFramework/Models/Person.cs
+++ EntityFramework/EntityFramework/Models/Person.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +10,24 @@ namespace EntityFramework.Models
 {
     public class Person
     {
+        // Id, ID, PersonId, PersonID <-- primary keys
+        // And if the data type is `int` or `guid`
+        // then EF will make the primary key an identity column.
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
         public DateTime BirthDate { get; set; }
         public decimal Weight { get; set; }
+        public bool IsStarWarsFan { get; set; }
+
+        // This property doesn't have a setter
+        // so EF will ignore it when generating the database.
+        public string Info
+        {
+            get
+            {
+                return $"{Name} ({BirthDate})";
+            }
+        }
     }
 }
--- EntityFramework/EntityFramework/Program.cs
+++ EntityFramework/EntityFramework/Program.cs
@@ -2,6 +2,7 @@
 using EntityFramework.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,17 +11,82 @@ namespace EntityFramework
 {
     class Program
     {
+        static bool Test(Person person)
+        {
+            return person.IsStarWarsFan;
+        }
+
         static void Main(string[] args)
         {
+            Database.SetInitializer(new DropCreateDatabaseIfModelChanges<Context>());
+            //Database.SetInitializer(new DropCreateDatabaseAlways<Context>());
+            //Database.SetInitializer(new CreateDatabaseIfNotExists<Context>());
+
             using (var context = new Context())
             {
-                context.People.Add(new Person()
+                //context.People.Add(new Person()
+                //{
+                //    Name = "James Churchill",
+                //    BirthDate = new DateTime(1968, 4, 22),
+                //    Weight = 215,
+                //    IsStarWarsFan = true
+                //});
+                //context.People.Add(new Person()
+                //{
+                //    Name = "Jenny Churchill",
+                //    BirthDate = new DateTime(1966, 5, 9),
+                //    Weight = 145,
+                //    IsStarWarsFan = false
+                //});
+                //context.SaveChanges();
+
+                // select * from People
+                //var people = context.People.ToList();
+
+                context.Database.Log = (message) => Console.WriteLine(message);
+
+
+                //var people = context.People.ToList();
+
+                ////Func<Person, bool> test = p => p.IsStarWarsFan;
+
+                //Func<Person, bool> test = (p) =>
+                //{
+                //    return p.IsStarWarsFan;
+                //};
+
+                //var filteredPeople = new List<Person>();
+
+                //foreach (var person in people)
+                //{
+                //    if (Test(person))
+                //    {
+                //        filteredPeople.Add(person);
+                //    }
+                //}
+
+                //var people2 = context.People
+                //    .Where(Test)
+                //    .ToList();
+
+
+
+                var people = context.People
+                    .Where(p => p.IsStarWarsFan || p.Name.Contains("j"))
+                    .OrderByDescending(p => p.Name)
+                    .ThenBy(p => p.BirthDate)
+                    .ThenByDescending(p => p.Weight)
+                    .ToList();
+
+                // Single, First, FirstOrDefault
+                var james = context.People
+                    .SingleOrDefault(p => p.Id == 1);
+
+
+                foreach (var person in people)
                 {
-                    Name = "James Churchill",
-                    BirthDate = new DateTime(1968, 4, 22),
-                    Weight = 215
-                });
-                context.SaveChanges();
+                    Console.WriteLine(person.Name);
+                }
             }
 
             Console.ReadKey();
