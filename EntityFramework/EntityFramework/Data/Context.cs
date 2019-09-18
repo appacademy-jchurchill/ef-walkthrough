ddfb782fbe30698f270bc97913a734f2f5356675
James Churchill <james@smashdev.com>
2019-09-18 06:34:32 -0700

    First pass at the data model

---
+++ EntityFramework/EntityFramework/Data/Context.cs
@@ -0,0 +1,15 @@
+﻿using EntityFramework.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework.Data
+{
+    public class Context : DbContext
+    {
+        public DbSet<Person> People { get; set; }
+    }
+}
---
+++ EntityFramework/EntityFramework/Program.cs
@@ -0,0 +1,29 @@
+﻿using EntityFramework.Data;
+using EntityFramework.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var context = new Context())
+            {
+                context.People.Add(new Person()
+                {
+                    Name = "James Churchill",
+                    BirthDate = new DateTime(1968, 4, 22),
+                    Weight = 215
+                });
+                context.SaveChanges();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
